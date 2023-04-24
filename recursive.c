8a24454f2bea6e26eb4a9f709d8dff94f1b7d137
bala262005 <128362727+bala262005@users.noreply.github.com>
2023-04-24 20:26:13 +0400

    Create recursive.c

---
+++ recursive.c
@@ -0,0 +1,23 @@
+// recursive function that prints from n to 0.
+
+#include <stdio.h>
+
+int rec(int m);
+
+int main() {
+    int n;
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    rec(n);
+    return 0;
+}
+
+int rec(int m){
+    if(m>0){
+        printf("%d",m-1);
+        return(rec(m-1));
+    }
+    else{
+        return 0;
+    }
+}
